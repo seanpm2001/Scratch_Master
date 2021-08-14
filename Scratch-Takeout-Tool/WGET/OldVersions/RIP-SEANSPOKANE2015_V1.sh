6b5a2f81a181380431bd6d6674890e2fab2ba29f
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-08-13 21:03:51 -0700

    RIP-SEANSPOKANE2015_V1.sh
    An archive of the first version of the experimental script to download my entire Scratch account.

---
+++ Scratch-Takeout-Tool/WGET/OldVersions/RIP-SEANSPOKANE2015_V1.sh
@@ -0,0 +1,17 @@
+# Start of script
+
+# This script is experimental and is for wget to rip all my Scratch data (as Scratch.mit.edu has no data export tool)
+# If successful, I will make a separate Scratch Takeout Tool
+# This project is currently incomplete and is not functional yet
+
+wget https://scratch.mit.edu/users/seanspokane2015/
+
+# End
+
+# File info
+
+# File type: Shell script file (*.sh)
+# File version: 1 (Friday, August 13th 2021 at 9:03 pm)
+# Line count (including blank lines and compiler line): 18
+
+# End of script
